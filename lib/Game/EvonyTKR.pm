d56b8ffa89cff1a425c1a9e994dc1276fef4e4e7
Luke Schierer <luke@schierer.org>
2024-07-15 14:03:02 -0400

    feat: data files for generals
    as I explore Gracile, I will still need data files for it to process if
    I do not want the same performance issues.  While it does server side
    processing more easily and better than Astro did, I still believe that
    pre-building will be critical.
    
    Perl will still probably be better suited to this task as it is pure
    file/data manipulation on the local file system.

---
+++ lib/Game/EvonyTKR.pm
@@ -0,0 +1,30 @@
+use 5.38.0;
+use strict;
+use warnings;
+package Game::EvonyTKR;
+
+=head1 NAME
+
+Game::EvonyTKR - Perl Modules providing utilities for players of Evony The King's Return.
+
+=head1 DESCRIPTION
+
+Serious players of the game will find themselves needing to do reasonably complex analysis to make optimal choices about 
+
+=over
+
+=item * Which generals to invest time and resources in
+
+=item * How to best pair generals for different senarios
+
+=item * The effects of Armor, Spiritual Beats, and Dragons on buffs and debuffs
+
+=item * Calculating your total buffs and debuffs in different senarios given the
+ever increasing number of sources for these buffs and debuffs
+
+=back
+
+This distribution aims to help players create and process this wealth of data. 
+=cut
+
+1;
