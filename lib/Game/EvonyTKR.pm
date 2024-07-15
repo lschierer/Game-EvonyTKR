b1a249f78ab1bff19ee491606dda2f7892570f12
Luke Schierer <luke@schierer.org>
2024-07-15 14:16:17 -0400

    feat: perl scaffolding try 2
    following https://github.com/sdondley/Dist-Zilla-for-Beginners/
    
    as of step 8

--- lib/Game/EvonyTKR.pm
+++ lib/Game/EvonyTKR.pm
@@ -2,6 +2,7 @@ use 5.38.0;
 use strict;
 use warnings;
 package Game::EvonyTKR;
+use base qw(App::Cmd::Simple);
 
 =head1 NAME
 
@@ -9,7 +10,7 @@ Game::EvonyTKR - Perl Modules providing utilities for players of Evony The King'
 
 =head1 DESCRIPTION
 
-Serious players of the game will find themselves needing to do reasonably complex analysis to make optimal choices about 
+Serious players of the game will find themselves needing to do reasonably complex analysis to make optimal choices about
 
 =over
 
@@ -27,4 +28,28 @@ ever increasing number of sources for these buffs and debuffs
 This distribution aims to help players create and process this wealth of data. 
 =cut
 
+sub opt_spec {
+  return (
+    [ "option1|a",  "do option 1" ],
+  );
+}
+
+sub validate_args {
+  my ($self, $opt, $args) = @_;
+
+  # no args allowed but options
+  $self->usage_error("No args allowed") if @$args;
+}
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  if ($opt->{option1}) {
+      # do option 1 stuff
+  } else {
+      # do regular stuff
+  }
+}
+
 1;
+
