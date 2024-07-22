acca6757f3c8a8cdc9c6068f25ac517aeb059972
Luke S. Schierer <luke@schierer.org>
2024-07-22 19:06:25 -0400

    feat: special skill books
    this should never have worked, it was missing a $ sign on a variable.
    
    Move all the POD to the bottom to aid in finding line numbers as well.

--- lib/Game/EvonyTKR/SkillBook/Special.pm
+++ lib/Game/EvonyTKR/SkillBook/Special.pm
@@ -7,13 +7,30 @@ class Game::EvonyTKR::SkillBook::Special :isa(Game::EvonyTKR::SkillBook) {
   use Type::Utils "is"; 
   use Game::EvonyTKR::Buff;
   use namespace::autoclean;
+
+  # from Type::Registry, this will save me from some of the struggles I have had with some types having blessed references and others not. 
+  ADJUST {
+    if(!(t->simple_lookup("Num"))) {
+      t->add_types(
+      -Common
+      );
+    }
+  }
+
+  field $builtIn :reader :param //= 1;
+
+  method is_built_in() {
+    return 1;
+  }
+  
+}
+
+1;
+__END__
+
 # PODNAME: Game::EvonyTKR::SkillBook::Special
 
 # ABSTRACT: Module for processing information about Evony TKR SkillBooks Built-in to each General.
-
-=head1 SYNOPSIS
-
-=for comment Brief examples of using the module.
 
 =head1 DESCRIPTION
 
@@ -25,29 +42,13 @@ While there is no particular logic here, by requiring this class, I ensure that 
 
 =cut 
 
-  # from Type::Registry, this will save me from some of the struggles I have had with some types having blessed references and others not. 
-  ADJUST {
-    if(!(t->simple_lookup("Num"))) {
-      t->add_types(
-      -Common
-      );
-    }
-  }
-
-=attr builtin
+=attr $builtIn
 
 Most of the time when dealing with SkillBook::Special, we are dealing with the general's built in skill book.  Occasionally we are dealing with a removable skill set that the game displays as a "skin" or alternative outfit/appearance for the general.  These alternative apperances come with buffs that behave functionally equivalent to an additional SkillBook. 
 =cut
-  field builtin :reader :param //= 1;
 
 =method is_builtin()
 
 an assertion that tells us if this is the built in skill or an extra this general has from an add-on.
 =cut
-  method is_builtin() {
-    return 1;
-  }
-  
-}
 
-1;
