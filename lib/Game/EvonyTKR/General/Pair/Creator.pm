1ec1524a2b3d25e023399f1beec51577d4b103c0
Luke S. Schierer <luke@schierer.org>
2024-07-22 21:01:09 -0400

    feat: create pairs
    start work with the native db, it won't help with the website, but the
    bdb option is just not going to deploy locally without hoops.

--- lib/Game/EvonyTKR/General/Pair/Creator.pm
+++ lib/Game/EvonyTKR/General/Pair/Creator.pm
@@ -3,13 +3,14 @@ use experimental qw(class);
 
 class Game::EvonyTKR::General::Pair {
   use Carp;
+  use Clone 'clone';
   use Types::Common qw( t is_Num is_Str is_Int);
   use Type::Utils "is"; 
   use File::ShareDir ':ALL';
   use File::HomeDir;
   use File::Spec;
   use File::Path qw(make_path);
-  use BerkeleyDB;  
+  use DBM::Deep;  
   use Game::EvonyTKR::SkillBook::Special;
   use Game::EvonyTKR::Buff::EvaluationMultipliers;
   use namespace::autoclean;
@@ -29,23 +30,36 @@ takes a hash of Game::EvonyTKR::General class objects and creates Game::EvonyTKR
       make_path($dbPath,"0770");
     }
   }
-  my $bdbEnv = new BerkeleyDB::Env
-    -Home   => $dbPath
-    -Flags  => DB_CREATE | DB_INIT_LOCK | DB_INIT_LOG | DB_INIT_MPOOL | DB_INIT_CDB | DB_INIT_TXN | DB_THREAD ;
+  my $db;
+  ADJUST {
+    $db = DBM::Deep->new(
+    files     => $dbPath,
+    locking   => 1,
+    autoflush => 1,
+    );
+  }
 
-  my $conflictDb = new BerkeleyDB::Hash
-    -Filename => "GeneralConflicts.db"
-    -Env      => $bdbEnv;
 
 =attr generals
 
 a hash of the generals to pair, using the name of the generals as the key. 
 =cut
-  field %general :param :reader; 
+  field %generals :reader; 
+
+  method set_generals( %ng ) {
+    if(scalar %ng >= 1) {
+      %generals = %{ clone (\%ng)}; 
+    }
+  }
   
   sub getConflictData() {
+    my $debug = 1;
     my $data_location = dist_file('Game-EvonyTKR', 'generalConflictGroups');
-
+    while (my $file = glob($data_location.'*.yaml')) {
+      if ($debug) {
+        say $file;
+      }
+    }
   }
 
 };
