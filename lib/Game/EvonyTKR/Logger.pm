52715e07de5b0a3cdd53de245186079a70adb6f4
Luke Schierer <luke@schierer.org>
2024-08-07 19:45:03 -0400

    feat: logging
    this configuration seems to do what I want more cleanly

--- lib/Game/EvonyTKR/Logger.pm
+++ lib/Game/EvonyTKR/Logger.pm
@@ -19,34 +19,14 @@ class Game::EvonyTKR::Logger {
   use Log::Log4perl;
   use namespace::autoclean;
 
+  field $category :reader :param = __CLASS__;
+
   field $logger :reader;
 
   field $location: reader;
 
   ADJUST {
-    if(not Log::Log4perl::initialized()) {
-      $location = $self->getLogfileName();
-      $self->_logInit();
-    }
-    $logger = Log::Log4perl->get_logger("Game::EvonyTKR");
-  }
-
-  method _logInit() {
-    my %conf = (
-      "log4perl.rootLogger"                   => "ALL, logFile",
-
-      "log4perl.appender.logFile"             => "Log::Log4perl::Appender::File",
-      "log4perl.appender.logFile.utf8"        => 1,
-      "log4perl.appender.logFile.filename"    => $location,
-      "log4perl.appender.Logfile.DatePattern" => "yyyy-MM-dd",
-      "log4perl.appender.Logfile.TZ"          => "UTC",
-      "log4perl.appender.logFile.mode"        => "append",
-      "log4perl.appender.logFile.layout"      => "Log::Log4perl::Layout::PatternLayout",
-      "log4perl.appender.logFile.layout.ConversionPattern" => "[%p] %d (%C line %L) %m%n",
-    );
-    # ... passed as a reference to init()
-    Log::Log4perl::init( \%conf );
-  
+    $logger = Log::Log4perl->get_logger($category);
   }
 
   method getLogfileName($name = 'system.log') {
