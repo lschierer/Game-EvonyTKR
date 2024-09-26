6675ebe87643ef88735b31196f23a15002d39dfb
Luke Schierer <lschiere@amazon.com>
2024-09-26 11:17:51 -0400

    custom logging works, but mojolicious appears to have next to no (if any) default logging

--- lib/Game/EvonyTKR/Web.pm
+++ lib/Game/EvonyTKR/Web.pm
@@ -7,10 +7,12 @@ use namespace::autoclean;
 
 package Game::EvonyTKR::Web {
 # ABSTRACT: package providing REST wrappers for the content created by this distribution
+  use Carp;
   use Mojo::Base 'Mojolicious', -signatures;
   use Mojo::File::Share qw(dist_dir dist_file);
   use FindBin;
   use lib "$FindBin::Bin/../../../lib";
+  use Game::EvonyTKR::Web::Logger;
 
   # This method will run once at server start
   sub startup ($self) {
@@ -20,11 +22,17 @@ package Game::EvonyTKR::Web {
     my $confFile = 
       File::Spec->catfile($dist_dir, 'game-evony_t_k_r-web.yml');
     
-    
     # Load configuration from config file
     my $config = $self->plugins->register_plugin(
       'Mojolicious::Plugin::NotYAMLConfig',
       Mojolicious->new, {file => $confFile} );
+
+    my $wlog = Game::EvonyTKR::Web::Logger->new(
+      category  => 'Web',
+      level     => $self->mode() eq 'production' ? 'info' : 'trace',
+    );
+    $wlog->logInit();
+    $self->log($wlog->logger());
 
     # Configure the application
     if(my $secrets = $config->{secrets}) {
@@ -38,7 +46,7 @@ package Game::EvonyTKR::Web {
     $r->get('/')->to('Example#welcome');
 
     $self->plugin('Game::EvonyTKR::Web::Routes::General', $r);
-
+    $self->log()->info('start up complete');
   }
 
 }
