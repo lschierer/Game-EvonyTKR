34d7be56f9913c3f267eda1b5f323bc7914bbd08
Luke S. Schierer <luke@schierer.org>
2024-09-26 17:31:16 -0400

    a non-vebose single-general json output

--- lib/Game/EvonyTKR/Web/Controller/General.pm
+++ lib/Game/EvonyTKR/Web/Controller/General.pm
@@ -8,12 +8,15 @@ package Game::EvonyTKR::Web::Controller::General {
   use Carp;
   use Data::Printer;
   use Devel::Peek;
+  use File::ShareDir ':ALL';
+  use File::Spec;
   use Util::Any ':all';
   use Game::EvonyTKR::General;
   use namespace::clean;
   use FindBin;
   use lib "$FindBin::Bin/../../../../../lib";
   use Mojo::Base 'Mojolicious::Controller', -signatures;
+  use Mojo::JSON qw(decode_json encode_json);
 
   sub index ($self) {
     $self->render(text => "Generals index Page");
@@ -25,7 +28,30 @@ package Game::EvonyTKR::Web::Controller::General {
 
   sub generalById($self) {
     my $id = $self->param('id');
-    $self->render();
+    $self->log()->trace("looking for $id in generalById");
+    
+    my $generalShare =
+      File::Spec->catfile(
+        File::ShareDir::dist_dir('Game-EvonyTKR'), 'generals');
+    my $FileWithPath = File::Spec->catfile($generalShare, "$id.yaml");
+
+    if( -T -s -r $FileWithPath) {
+      $self->log()->trace("found $id in generalById");
+      my $general = Game::EvonyTKR::General->new(
+        name  => $id,
+      );
+      $general->readFromFile();
+      my $generalHashRef = $general->toHashRef();
+
+      $self->respond_to(
+        json  => {json => $generalHashRef} ,
+        any   => { data => '', status => 204},
+      );
+    }
+    else {
+      $self->reply->not_found();
+    }
+    
   }
 }
 1;
