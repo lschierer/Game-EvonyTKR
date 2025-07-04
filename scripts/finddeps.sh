1ab1240113280fbdf7f01e65ee7ded4e46f011ed
Luke S. Schierer <luke@schierer.org>
2025-07-03 21:09:55 -0400

    build: housekeeping

--- scripts/finddeps.sh
+++ scripts/finddeps.sh
@@ -2,10 +2,12 @@
 
  step1=$(find lib/ scripts/ -type f -exec egrep -h '^\s*(use|require)' {} \;) || exit 1
 
- step2=$(echo "$step1" | sed 's/^[ \t]*//' | cut -d ' ' -f 2 | cut -d ';' -f 1) || exit 2
+ step2=$(echo "$step1" | grep -v required ) || exit 2
 
- step3=$(echo "$step2" | sort -u ) || exit 3
+ step3=$(echo "$step1" | sed 's/^[ \t]*//' | cut -d ' ' -f 2 | cut -d ';' -f 1) || exit 3
 
- step4=$(echo "$step3" | egrep -v '^(v5.40|feature|parent|overload|experimental|Game::EvonyTKR|utf8)') || exit 4
+ step4=$(echo "$step2" | sort -u ) || exit 4
 
- echo "$step4"
+ step5=$(echo "$step3" | egrep -v '^(v5.40|feature|parent|overload|experimental|Game::EvonyTKR|utf8)') || exit 5
+
+ echo "$step5"
