618e503356b52cf1bbef7e1537af636dc8717f30
Luke Schierer <luke@schierer.org>
2025-12-17 00:37:22 -0500

    the last 4 rows didn't get populated, I think this is why I did that long delay.

--- lib/Game/EvonyTKR/Controller/Pairs.pm
+++ lib/Game/EvonyTKR/Controller/Pairs.pm
@@ -617,7 +617,7 @@ package Game::EvonyTKR::Controller::Pairs {
           # run in, I *need* this one to be *after* all the individual
           # job handlers have run.
           Mojo::IOLoop->timer(
-            0.1 => sub ($loop) {
+            10 => sub ($loop) {
               $c->log_debug(
                 'all jobs complete promise handler sending complete event');
               my $payload = $c->encode({ runId => $run_id });
