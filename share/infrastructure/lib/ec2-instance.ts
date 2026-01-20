160064bed1b435c9d463977f48a72d72f92f7ae9
Luke Schierer <luke@schierer.org>
2026-01-20 04:02:27 -0500

    trying to consolidate ec2 config info from two places to one, to debug why the init is not working

--- share/infrastructure/lib/ec2-instance.ts
+++ share/infrastructure/lib/ec2-instance.ts
@@ -1,23 +1,17 @@
 import { NestedStack, Stack } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 //import * as iam from 'aws-cdk-lib/aws-iam';
-import * as s3Assets from 'aws-cdk-lib/aws-s3-assets';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 import * as cloudwatch_actions from 'aws-cdk-lib/aws-cloudwatch-actions';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as fs from 'fs';
 import * as cdk from 'aws-cdk-lib';
-import * as yaml from 'js-yaml';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { CustomUbuntuUserData } from './userdata';
 
 import { type MojoliciousStackProps } from './main-stack';
 
-interface UbuntuInstanceProps extends MojoliciousStackProps {
+export interface UbuntuInstanceProps extends MojoliciousStackProps {
   vpc: ec2.IVpc | ec2.Vpc;
 }
 
@@ -45,113 +39,11 @@ export class UbuntuInstance extends NestedStack {
       props.environment === 'prod'
         ? `www${this.getRandomInteger(10, 99)}`
         : `${props.appSubdomain}${this.getRandomInteger(10, 99)}`;
-    let userDataFile = fs.readFileSync(
-      path.join(__dirname, '../etc/user-data.yaml'),
-      'utf8',
-    );
-    userDataFile = userDataFile.replaceAll('REPLACEHOSTNAME', this.hostname);
-    userDataFile = userDataFile.replaceAll(
-      'REPLACE2.',
-      props.environment === 'prod' ? '' : `${props.environment}2.`,
-    );
-    userDataFile = userDataFile.replaceAll(
-      'REPLACEMODE',
-      props.environment === 'prod' ? 'production' : 'staging',
-    );
 
-    const cloud_user_data: Record<string, unknown> = yaml.load(
-      userDataFile,
-    ) as Record<string, unknown>;
-    cloud_user_data.runcmd =
-      cloud_user_data['runcmd' as keyof typeof cloud_user_data] || [];
+    const custominit = new CustomUbuntuUserData(this, props);
 
-    const prefixBinAsset = new s3Assets.Asset(this, 'prefixBinAsset', {
-      path: path.join(__dirname, '../prefixBin'),
-    });
-    prefixBinAsset.grantRead(instanceRole);
-
-    const prefix_etc_asset = new s3Assets.Asset(this, 'prefix_etc_asset', {
-      path: path.join(__dirname, '../etc'),
-    });
-
-    const shellCommands = ec2.UserData.forLinux();
-    shellCommands.addCommands(
-      'curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"',
-      'unzip awscliv2.zip',
-      'sudo ./aws/install',
-    );
-
-    const local_bin_path = shellCommands.addS3DownloadCommand({
-      bucket: prefixBinAsset.bucket,
-      bucketKey: prefixBinAsset.s3ObjectKey,
-    });
-
-    const local_etc_path = shellCommands.addS3DownloadCommand({
-      bucket: prefix_etc_asset.bucket,
-      bucketKey: prefix_etc_asset.s3ObjectKey,
-    });
-
-    const hostprefix =
-      props.environment === 'prod' ? 'production' : props.environment;
-    shellCommands.addCommands(
-      'mkdir -p /opt/prefix/bin',
-      'cd /opt/prefix/bin',
-      `unzip ${local_bin_path}`,
-      'chown -R appuser:www-data /opt/prefix',
-      'chmod +x /opt/prefix/bin/*.sh',
-      'mv .bash* /opt/prefix/',
-      'cp /opt/prefix/bin/deploy-prefix.sh /usr/local/bin',
-      'chmod 0755 /opt/prefix/bin/deploy-prefix.sh',
-      `sed -i -E 's/replace/${hostprefix}/' /opt/prefix/bin/setup-cert.sh`,
-      'cp /opt/prefix/bin/setup-cert.sh /usr/local/bin',
-      'chmod 0755 /usr/local/bin/setup-cert.sh',
-      'mkdir -p /tmp/prefix_etc',
-      'cd /tmp/prefix_etc',
-      `unzip ${local_etc_path}`,
-      'sudo cp /tmp/prefix_etc/setup-cert.service /etc/systemd/system/',
-      'sudo cp /tmp/prefix_etc/evonytkr.service /etc/systemd/system/',
-      'sudo cp /tmp/prefix_etc/sysctl.conf /etc/sysctl.d/evonytkr.conf',
-      'mkdir -p /opt/prefix/.local/share/mise/',
-      'sudo cp /tmp/prefix_etc/trusted.toml /opt/prefix/.local/share/mise/trusted.toml',
-      'sudo chown -R appuser:appuser /opt/prefix/.local',
-      'sudo service procps force-reload',
-    );
-
-    // Generate mode-specific config BEFORE bootstrap (git clone happens in bootstrap)
-    // Created in /opt/prefix/etc, then bootstrap.sh copies it to /opt/prefix/app after clone
-    const appMode = props.environment === 'prod' ? 'production' : 'staging';
-    const configFileName = `game-evony_t_k_r.${appMode}.yml`;
-    console.log(`config file is ${configFileName}`);
-    shellCommands.addCommands(
-      'mkdir -p /opt/prefix/etc',
-      'chown appuser:www-data /opt/prefix/etc',
-      'cd /opt/prefix/etc',
-    );
-
-    // Run bootstrap and signal success/failure to CloudFormation
-    // bootstrap.sh will: 1) git clone, 2) copy config from /opt/prefix/etc to /opt/prefix/app
-    shellCommands.addCommands(
-      'set +e', // Don't exit on error so we can signal failure
-      'sudo -u appuser -s /bin/bash -l -c /opt/prefix/bin/bootstrap.sh',
-      'BOOTSTRAP_EXIT_CODE=$?',
-      'set -e',
-    );
-
-    shellCommands.addCommands(
-      'sudo systemctl enable setup-cert.service',
-      'systemctl start setup-cert.service',
-      'sudo /opt/prefix/bin/setup-nginx.sh',
-      'sudo systemctl enable evonytkr.service',
-      'systemctl start evonytkr',
-      'systemctl reload nginx',
-    );
-
-    (cloud_user_data.runcmd as Array<string>).push(shellCommands.render());
-
-    // Combine them with MultiPart
-    const userData = ec2.UserData.custom(
-      `#cloud-config\n${yaml.dump(cloud_user_data)}`,
-    );
+    custominit.prefix_etc_asset.grantRead(instanceRole);
+    custominit.prefix_bin_asset.grantRead(instanceRole);
 
     const instanceSize = !props.environment.localeCompare('dev')
       ? ec2.InstanceSize.LARGE
@@ -167,8 +59,9 @@ export class UbuntuInstance extends NestedStack {
 
     this.instance = new ec2.Instance(this, 'Instance', {
       role: instanceRole,
-      userData,
       userDataCausesReplacement: true,
+      init: custominit.init,
+      initOptions: custominit.initOptions,
       vpc: props.vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, instanceSize),
       securityGroup: ec2SecGroup,
---
+++ share/infrastructure/lib/userdata.ts
@@ -0,0 +1,293 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+//import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3Assets from 'aws-cdk-lib/aws-s3-assets';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import * as cdk from 'aws-cdk-lib';
+import { Stack } from 'aws-cdk-lib';
+
+import { type UbuntuInstanceProps } from './ec2-instance';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export class CustomUbuntuUserData {
+  public init: ec2.CloudFormationInit;
+
+  public initOptions: ec2.ApplyCloudFormationInitOptions;
+
+  public prefix_bin_asset: s3Assets.Asset;
+
+  public prefix_etc_asset: s3Assets.Asset;
+
+  public constructor(stack: Stack, props: UbuntuInstanceProps) {
+    const hostname =
+      props.environment === 'prod'
+        ? `www${this.getRandomInteger(10, 99)}`
+        : `${props.appSubdomain}${this.getRandomInteger(10, 99)}`;
+
+    this.prefix_bin_asset = new s3Assets.Asset(stack, 'prefixBinAsset', {
+      path: path.join(__dirname, '../prefixBin'),
+    });
+
+    this.prefix_etc_asset = new s3Assets.Asset(stack, 'prefix_etc_asset', {
+      path: path.join(__dirname, '../etc'),
+    });
+
+    const shellCommands = ec2.UserData.forLinux();
+    shellCommands.addCommands(
+      'curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"',
+      'unzip awscliv2.zip',
+      'sudo ./aws/install',
+    );
+
+    const local_bin_path = shellCommands.addS3DownloadCommand({
+      bucket: this.prefix_bin_asset.bucket,
+      bucketKey: this.prefix_bin_asset.s3ObjectKey,
+    });
+
+    const local_etc_path = shellCommands.addS3DownloadCommand({
+      bucket: this.prefix_etc_asset.bucket,
+      bucketKey: this.prefix_etc_asset.s3ObjectKey,
+    });
+
+    const hostprefix =
+      props.environment === 'prod' ? 'production' : props.environment;
+
+    // Generate mode-specific config BEFORE bootstrap (git clone happens in bootstrap)
+    // Created in /opt/prefix/etc, then bootstrap.sh copies it to /opt/prefix/app after clone
+    const appMode = props.environment === 'prod' ? 'production' : 'staging';
+    const configFileName = `game-evony_t_k_r.${appMode}.yml`;
+    console.log(`config file is ${configFileName}`);
+    shellCommands.addCommands();
+
+    this.init = ec2.CloudFormationInit.fromConfigSets({
+      configSets: {
+        default: [
+          'installPackages',
+          'createUsers',
+          'configureSSM',
+          'configureApp',
+        ],
+      },
+      configs: {
+        installPackages: new ec2.InitConfig([
+          ec2.InitCommand.shellCommand('add-apt-repository -y universe'),
+          ec2.InitCommand.shellCommand('apt-get update'),
+          ec2.InitCommand.shellCommand('apt-get dist-upgrade -y'),
+          ec2.InitPackage.apt('autoconf'),
+          ec2.InitPackage.apt('autoconf-archive'),
+          ec2.InitPackage.apt('automake'),
+          ec2.InitPackage.apt('bison'),
+          ec2.InitPackage.apt('build-essential'),
+          ec2.InitPackage.apt('certbot'),
+          ec2.InitPackage.apt('curl'),
+          ec2.InitPackage.apt('flex'),
+          ec2.InitPackage.apt('gcc'),
+          ec2.InitPackage.apt('gcc-13-locales'),
+          ec2.InitPackage.apt('gettext'),
+          ec2.InitPackage.apt('git'),
+          ec2.InitPackage.apt('gnu-standards'),
+          ec2.InitPackage.apt('graphviz'),
+          ec2.InitPackage.apt('libgd-dev'),
+          ec2.InitPackage.apt('libgraphviz-dev'),
+          ec2.InitPackage.apt('libssl-dev'),
+          ec2.InitPackage.apt('libtool'),
+          ec2.InitPackage.apt('libzip-dev'),
+          ec2.InitPackage.apt('make'),
+          ec2.InitPackage.apt('nginx'),
+          ec2.InitPackage.apt('openssh-server'),
+          ec2.InitPackage.apt('pipx'),
+          ec2.InitPackage.apt('pkg-config'),
+          ec2.InitPackage.apt('plocate'),
+          ec2.InitPackage.apt('postgresql'),
+          ec2.InitPackage.apt('postgresql-contrib'),
+          ec2.InitPackage.apt('postgresql-server-dev-16'),
+          ec2.InitPackage.apt('python3'),
+          ec2.InitPackage.apt('python3-certbot-nginx'),
+          ec2.InitPackage.apt('python3-pip'),
+          ec2.InitPackage.apt('rsync'),
+          ec2.InitPackage.apt('sqlite3'),
+          ec2.InitPackage.apt('sysstat'),
+          ec2.InitPackage.apt('unattended-upgrades'),
+          ec2.InitPackage.apt('unzip'),
+          ec2.InitPackage.apt('update-notifier-common'),
+          ec2.InitCommand.shellCommand('systemctl is-active sysstat'),
+          ec2.InitCommand.shellCommand('systemctl enable unattended-upgrades'),
+        ]),
+        createUsers: new ec2.InitConfig([
+          ec2.InitUser.fromName('appuser', {
+            homeDir: '/opt/prefix',
+            groups: ['www-data'],
+          }),
+          ec2.InitCommand.shellCommand(
+            '/usr/bin/chsh -s /sbin/nologin appuser',
+          ),
+          ec2.InitUser.fromName('luke', {
+            groups: [
+              'users',
+              'admin',
+              'adm',
+              'man',
+              'sudo',
+              'operator',
+              'src',
+              'staff',
+              'crontab',
+              'www-data',
+            ],
+          }),
+          // TODO: set up sudo for 'luke' user.
+          // TODO: set up ssh_authorized_keys for 'luke' user.
+          ec2.InitCommand.shellCommand(
+            ' /usr/sbin/groupmod -a appuser -U luke',
+          ),
+        ]),
+        configureSSM: new ec2.InitConfig([
+          ec2.InitCommand.shellCommand('apt-get remove --purge -y snapd'),
+          ec2.InitCommand.shellCommand(
+            'curl "https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_arm64/amazon-ssm-agent.deb" -o "/tmp/amazon-ssm-agent.deb"',
+          ),
+          ec2.InitCommand.shellCommand('dpkg -i /tmp/amazon-ssm-agent.deb'),
+          ec2.InitCommand.shellCommand('systemctl enable amazon-ssm-agent'),
+          ec2.InitCommand.shellCommand('systemctl start amazon-ssm-agent'),
+        ]),
+        configureApp: new ec2.InitConfig([
+          ec2.InitFile.fromObject('/etc/stack.json', {
+            stackId: Stack.of(stack).stackId,
+            stackName: Stack.of(stack).stackName,
+            region: Stack.of(stack).region,
+          }),
+          ec2.InitCommand.shellCommand(`hostname ${hostname}`),
+          ec2.InitCommand.shellCommand(`hostnamectl set-hostname ${hostname}`),
+          ec2.InitCommand.shellCommand(
+            `echo "127.0.1.1 ${hostname} ${hostname}.localdoamin" >> /etc/hosts`,
+          ),
+          ec2.InitCommand.shellCommand(
+            `PIPX_HOME=/usr/local/ PIPX_BIN_DIR=/usr/local/bin PIPX_MAN_DIR=/usr/local/ /usr/bin/pipx install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz || echo 'cfn-bootstrap failed' >> /var/log/boostrap.log`,
+          ),
+          ec2.InitCommand.shellCommand(
+            'ln -sf /usr/local/bin/cfn-* /usr/bin/ || true',
+          ),
+          ec2.InitCommand.shellCommand('mkdir -p /opt/prefix/var/run'),
+          ec2.InitCommand.shellCommand('mkdir -p /opt/prefix/var/log'),
+          ec2.InitCommand.shellCommand('mkdir -p /opt/prefix/bin'),
+          ec2.InitCommand.shellCommand('mkdir -p /opt/prefix/etc'),
+          ec2.InitCommand.shellCommand('chown -R appuser:www-data /opt/prefix'),
+          ec2.InitCommand.shellCommand(
+            'sudo -u appuser touch /opt/prefix/.bash_profile',
+          ),
+          ec2.InitCommand.shellCommand(
+            `echo 'export PATH="/opt/prefix/.local/bin/:$HOME/bin:$PATH"' >> /opt/prefix/.bash_profile`,
+          ),
+          ec2.InitCommand.shellCommand('mkdir /etc/nginx/ssl'),
+          ec2.InitCommand.shellCommand(
+            'openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/ssl/evonytkrtips.net.key -out /etc/nginx/ssl/evonytkrtips.net.crt -days 365 -nodes -subj "/C=US/ST=State/L=City/O=Org/OU=OrgUnit/CN=localhost"',
+          ),
+          // if there is not a cert present where the nginx config states, then
+          // when certbot runs, it checks the nginx config syntax for correctness and it will fail
+          ec2.InitCommand.shellCommand(
+            'chown -R www-data:www-data /etc/nginx/ssl',
+          ),
+          ec2.InitCommand.shellCommand(
+            'sync && echo 3 > /proc/sys/vm/drop_caches',
+          ),
+          ec2.InitCommand.shellCommand(
+            'mkdir -p /opt/prefix/var/log/Perl/dist/Game-EvonyTKR/',
+          ),
+          ec2.InitCommand.shellCommand(
+            'mkdir -p /home/luke/var/log/Perl/dist/Game-EvonyTKR/',
+          ),
+          ec2.InitCommand.shellCommand(
+            'chown -R appuser:www-data /opt/prefix/var',
+          ),
+          ec2.InitCommand.shellCommand('chown -R luke:luke /home/luke/var'),
+          ec2.InitCommand.shellCommand(
+            'find /opt/prefix/var -type d -exec chmod g+rx {} \;',
+          ),
+          ec2.InitCommand.shellCommand('cd /opt/prefix/bin'),
+          ec2.InitCommand.shellCommand(`unzip ${local_bin_path}`),
+          ec2.InitCommand.shellCommand('chown -R appuser:www-data /opt/prefix'),
+          ec2.InitCommand.shellCommand('chmod +x /opt/prefix/bin/*.sh'),
+          ec2.InitCommand.shellCommand('mv .bash* /opt/prefix/'),
+          ec2.InitCommand.shellCommand(
+            'mkdir -p /opt/prefix/.local/share/mise/',
+          ),
+          ec2.InitCommand.shellCommand(
+            'sudo cp /tmp/prefix_etc/trusted.toml /opt/prefix/.local/share/mise/trusted.toml',
+          ),
+          ec2.InitCommand.shellCommand(
+            'sudo chown -R appuser:appuser /opt/prefix/.local',
+          ),
+
+          ec2.InitCommand.shellCommand(
+            'sudo -u appuser -s /bin/bash -l -c /opt/prefix/bin/bootstrap.sh',
+          ),
+          ec2.InitCommand.shellCommand(
+            'echo "BOOTSTRAP_EXIT_CODE=$?" | sudo tee /var/log/cloud-output-init.log',
+          ),
+
+          ec2.InitCommand.shellCommand(
+            'cp /opt/prefix/bin/deploy-prefix.sh /usr/local/bin',
+          ),
+          ec2.InitCommand.shellCommand(
+            'chmod 0755 /opt/prefix/bin/deploy-prefix.sh',
+          ),
+
+          ec2.InitCommand.shellCommand(
+            `sed -i -E 's/replace/${hostprefix}/' /opt/prefix/bin/setup-cert.sh`,
+          ),
+          ec2.InitCommand.shellCommand(
+            'cp /opt/prefix/bin/setup-cert.sh /usr/local/bin',
+          ),
+          ec2.InitCommand.shellCommand(
+            'chmod 0755 /usr/local/bin/setup-cert.sh',
+          ),
+          ec2.InitCommand.shellCommand('mkdir -p /tmp/prefix_etc'),
+          ec2.InitCommand.shellCommand('cd /tmp/prefix_etc'),
+          ec2.InitCommand.shellCommand(`unzip ${local_etc_path}`),
+          ec2.InitCommand.shellCommand(
+            'sudo cp /tmp/prefix_etc/setup-cert.service /etc/systemd/system/',
+          ),
+
+          ec2.InitCommand.shellCommand(
+            'sudo cp /tmp/prefix_etc/sysctl.conf /etc/sysctl.d/evonytkr.conf',
+          ),
+          ec2.InitCommand.shellCommand('sudo service procps force-reload'),
+
+          ec2.InitCommand.shellCommand(
+            'sudo cp /tmp/prefix_etc/evonytkr.service /etc/systemd/system/',
+          ),
+
+          ec2.InitCommand.shellCommand(
+            'cp /tmp/prefix_etc/logrotate-evonytkr.conf /etc/logrotate.d/evonytkr',
+          ),
+          ec2.InitCommand.shellCommand('chmod 644 /etc/logrotate.d/evonytkr'),
+
+          ec2.InitCommand.shellCommand(
+            'sudo systemctl enable setup-cert.service',
+          ),
+          ec2.InitCommand.shellCommand('systemctl start setup-cert.service'),
+          ec2.InitCommand.shellCommand('sudo /opt/prefix/bin/setup-nginx.sh'),
+          ec2.InitCommand.shellCommand(
+            'sudo systemctl enable evonytkr.service',
+          ),
+          ec2.InitCommand.shellCommand('systemctl start evonytkr'),
+          ec2.InitCommand.shellCommand('systemctl reload nginx'),
+        ]),
+      },
+    });
+
+    this.initOptions = {
+      configSets: ['default'],
+      timeout: cdk.Duration.minutes(30),
+    };
+  }
+
+  getRandomInteger(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
