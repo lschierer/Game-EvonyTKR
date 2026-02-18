bab6c36da21681ffe2600989e953ef917cc48e49
Luke Schierer <luke@schierer.org>
2026-02-18 00:54:39 -0500

    fix: relax the type rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,13 @@
 export default {
   extends: [
     '@commitlint/config-conventional'
-  ]
+  ],
+  /*
+   * type-enum is disabled so any type keyword is accepted (not just the
+   * conventional-commits list). The structure "type: subject" is still
+   * enforced by the other rules (type-empty, subject-empty, etc).
+   */
+  rules: {
+    'type-enum': [0],
+  },
 };
